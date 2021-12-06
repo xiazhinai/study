4bbb8cce79a04dcd7268cc1db2e8297b4bfa03c9
xiazhinai <1004823433@qq.com>
2021-12-06 16:05:38 +0800

    测试list的坑

--- src/main/java/com/example/demo/test/Test03.java
+++ src/main/java/com/example/demo/test/Test03.java
@@ -1,10 +1,31 @@
 package com.example.demo.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test03 {
 
-
-
-    public void test(){
-        System.out.println(1);
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i)==3){
+                list.remove(i);
+            }
+        }
+        System.out.println(list);
+        Integer a = 128;
+        Integer b = 127;
+        System.out.println(a==b);
+        new Test03().main();
+    }
+    Integer a  = 222;
+    public  void main() {
+        System.out.println(a ==222);
     }
 }
