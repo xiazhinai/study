4c46dc8473b41f49f21115c5e9387a734e2df69a
xiazhinai <1004823433@qq.com>
2021-12-27 14:32:04 +0800

    1

---
+++ src/main/java/com/example/demo/test/Test06.java
@@ -0,0 +1,19 @@
+package com.example.demo.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author xiazhinai
+ * @description 测试原子类
+ * @date 2021/12/23 20:11
+ */
+public class Test06 {
+
+
+    public static void main(String[] args) {
+        AtomicInteger ac = new AtomicInteger(0);
+    }
+
+
+
+}
