0a3f08b4bae9d1f790c4952608de44ea1489e6a9
evan <1004823433@qq.com>
2022-07-25 15:03:20 +0800

    11

---
+++ src/main/java/com/example/demo/test/Test12.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+/**
+ * @author xiazhinai
+ */
+public class Test12 {
+
+    public static void main(String[] args) {
+        System.out.println(1<<30>2<<29);
+        System.out.println(1<<30==2<<29);
+        System.out.println(1<<5);
+    }
+
+}
