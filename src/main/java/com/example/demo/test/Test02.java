12763daded3c79a8409a421596bb889d13efa4fd
luoyunjian <1004823433@qq.com>
2022-12-06 15:46:16 +0800

    测试

--- src/main/java/com/example/demo/test/Test02.java
+++ src/main/java/com/example/demo/test/Test02.java
@@ -1,8 +1,16 @@
 package com.example.demo.test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+
 public class Test02 {
 
     public static void main(String[] args) {
         System.out.println(String.valueOf("sgs").contains("sg"));
+        System.out.println(Timestamp.valueOf("2022-04-09 19:06:28").getTime());
+
+        BigDecimal bigDecimal = new BigDecimal((String)null).setScale(2, RoundingMode.HALF_UP);
+        System.out.println(bigDecimal);
     }
 }
