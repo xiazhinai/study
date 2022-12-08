b3a49388b454b33bbf0c97bbac08fbea346a40ea
luoyunjian <1004823433@qq.com>
2022-12-08 09:40:27 +0800

    测试

---
+++ src/main/java/com/example/demo/test/Test13.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+public class Test13 {
+
+    public static void main(String[] args) {
+        System.out.println(-1%3);
+        System.out.println(-15 % 6);
+        System.out.println(15 % -6);
+        System.out.println(-15 % -6);
+        Integer a = 2<<31;
+        System.out.println(2<<3);
+
+    }
+}
