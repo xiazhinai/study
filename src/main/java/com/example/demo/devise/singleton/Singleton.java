8baa3473d32fb964a525e732dbf8a600aa8def9c
luoyunjian <1004823433@qq.com>
2023-01-06 13:56:00 +0800

    测试

---
+++ src/main/java/com/example/demo/devise/singleton/Singleton.java
@@ -0,0 +1,6 @@
+package com.example.demo.devise.singleton;
+
+public class Singleton {
+
+
+}
--- src/main/java/com/example/demo/test/Test07.java
+++ src/main/java/com/example/demo/test/Test07.java
@@ -43,7 +43,7 @@ public class Test07 {
     /**
      * 向指定URL发送GET方法的请求
      *
-     * @param url
+     * @param url ,m..... m,m,,,
      *            发送请求的URL
      * @param param
      *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
