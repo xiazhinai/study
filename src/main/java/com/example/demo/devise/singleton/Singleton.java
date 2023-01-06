c478c87e319320e30518c669afa77710861bf8ce
luoyunjian <1004823433@qq.com>
2023-01-06 14:21:06 +0800

    Singleton 懒汉实现

--- src/main/java/com/example/demo/devise/singleton/Singleton.java
+++ src/main/java/com/example/demo/devise/singleton/Singleton.java
@@ -1,6 +1,28 @@
 package com.example.demo.devise.singleton;
 
+
+//单例实现---最简单实现 --饿汉模式
 public class Singleton {
 
+    //私有构造函数 使其他的所有对象操作的Singleton的实例是一个,
+    //就是下面的singleton
+    private Singleton() {
+    }
 
+    static Singleton singleton;
+
+    //获得单例的唯一实例
+    //当其他对象需要操作该类的方法时,因为没法自己new生成 需要一个公共的函数来获得这个类的实例
+    public static Singleton getSingleton() {
+        //当有对象调用的时候才会真正的生成对象.不然就不会生成. ---懒加载  但是系统特高并发情况下不可用这种写法
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+    //具体的业务方法 此处假设是保存订单
+    public void saveOrder(){
+        System.out.println("订单保存成功");
+    }
 }
--- src/main/java/com/example/demo/devise/singleton/Test.java
+++ src/main/java/com/example/demo/devise/singleton/Test.java
@@ -5,11 +5,15 @@ package com.example.demo.devise.singleton;
  * 单例模式
  */
 public class Test {
-    private Test(){}
 
-    private  Test test;
+    public static void main(String[] args) {
+        //Singleton singleton1 = new Singleton(); //该代码会报错. 单例的本质就是让你无法新建多个重复的实体对象.减少消耗.
 
-    public static void sss(){
-
+        Singleton singleton2 = Singleton.getSingleton();
+        Singleton singleton3 = Singleton.getSingleton();
+        System.out.println("singleton2== "+singleton2);
+        System.out.println("singleton3== "+singleton3);
+        System.out.println("singleton2==singleton3 结果:" +(singleton2==singleton3));
     }
+
 }
